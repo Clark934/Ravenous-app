780804cc1377c55c6ff4f97d59bde622766a8be0
Clark934 <shbscsy@outlook.com>
2019-11-26 20:26:15 -0500

    Refactored Business and BusinessList

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
-import { ReactComponent } from '*.svg';
+
 
 class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
+                {this.props.businesses.map(
+                    (business) => {
+                        return <Business business={business} />;
+                    }
+                )}
             </div>
         );
     }
