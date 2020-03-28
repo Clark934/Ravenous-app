fb44f38a6c74c1e2086c7737c8643d1948f2fff7
Clark934 <shbscsy@outlook.com>
2020-03-27 22:09:39 -0400

    Linked address to Google Map

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,21 +2,31 @@ import React from 'react';
 import './Business.css';
 
 class Business extends React.Component {
+    constructor(props) {
+        super(props);
+        this.address = `${this.props.business.address}, ${this.props.business.city} ${this.props.business.zipCode}`;
+    }
     render() {
         return (
             <div className="Business">
                 <div className="image-container">
                     <a href={this.props.business.url} target="_blank" rel="noopener noreferrer">
-                    <img src={this.props.business.imageSrc} alt='' />
+                        <img src={this.props.business.imageSrc} alt='' />
                     </a>
                 </div>
                 <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
-                    <div className="Business-address">
-                        <p>{this.props.business.address}</p>
-                        <p>{this.props.business.city}</p>
-                        <p>{this.props.business.city + " " + this.props.business.zipCode}</p>
-                    </div>
+                    <a href={`https://maps.google.com/?q=${this.address}"`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ "text-decoration": "none" , "color": "black"}}
+                    >
+                        <div className="Business-address">
+                            <p>{this.props.business.address}</p>
+                            <p>{this.props.business.city}</p>
+                            <p>{this.props.business.city + " " + this.props.business.zipCode}</p>
+                        </div>
+                    </a>
                     <div className="Business-reviews">
                         <h3>{this.props.business.category.toUpperCase()}</h3>
                         <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
